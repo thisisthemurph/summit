642fd191d67d0d8ab9670c69500f07d852e3cce8
Mike Murphy <michael.w.murphy@outlook.com>
2024-07-14 13:56:49 +0100

    Removes use of container component

--- ui/src/shared/components/PageHeader.tsx
+++ ui/src/shared/components/PageHeader.tsx
@@ -1,5 +1,3 @@
-import Container from "./Container.tsx";
-
 type PageHeaderProps = {
   title: string | null | undefined;
   subtitle?: string;
@@ -7,13 +5,11 @@ type PageHeaderProps = {
 
 function PageHeader({title, subtitle}: PageHeaderProps) {
   return (
-    <section className="mb-6 shadow-lg">
-      <Container>
+    <section className="mb-6 shadow-lg p-4">
         <h2 className="text-2xl font-semibold">{title}</h2>
         {subtitle && (
           <p className="text-sm text-slate-800">{subtitle}</p>
         )}
-      </Container>
     </section>
   )
 }
