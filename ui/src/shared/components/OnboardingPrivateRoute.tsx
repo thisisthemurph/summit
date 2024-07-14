4948402a30eb2bb0b638e8c917bfa51281603d80
Mike Murphy <michael.w.murphy@outlook.com>
2024-07-14 12:33:58 +0100

    Adds comments

--- ui/src/shared/components/OnboardingPrivateRoute.tsx
+++ ui/src/shared/components/OnboardingPrivateRoute.tsx
@@ -1,6 +1,10 @@
 import {useAuth} from "../../hooks/useAuth.tsx";
 import {Navigate, Outlet} from "react-router-dom";
 
+/**
+ * A private route for the onboarding pages.
+ * The user must be authenticated, but they do not have to have their profile completed.
+ */
 const OnboardingPrivateRoute = () => {
   const { isAuthenticated } = useAuth()
 
--- ui/src/shared/components/PrivateRoute.tsx
+++ ui/src/shared/components/PrivateRoute.tsx
@@ -2,6 +2,11 @@ import {useAuth} from "../../hooks/useAuth.tsx";
 import {Navigate, Outlet} from "react-router-dom";
 import {AuthenticatedUser} from "../types/responseTypes.ts";
 
+/**
+ * A generic private route.
+ * Users must be authenticated and have completed the onboarding process.
+ * If the onboarding process is incomplete, they are navigated to the onboarding/profile page.
+ */
 const PrivateRoute = () => {
   const { isAuthenticated , authenticatedUser} = useAuth()
 
